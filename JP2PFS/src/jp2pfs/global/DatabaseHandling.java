f318bc46dc91811ff3a50e2b83a7c8ffeb688b90
middelma <middelma@PCR04-008.spe-mch.de>
2013-02-07 14:24:02 +0100

    abstract DatabaseHandling added

--- JP2PFS/src/jp2pfs/global/DatabaseHandling.java
+++ JP2PFS/src/jp2pfs/global/DatabaseHandling.java
@@ -3,11 +3,20 @@
  * and open the template in the editor.
  */
 package jp2pfs.global;
+import java.sql.*;
 
 /**
  *
  * @author karlinsv
  */
 public abstract class DatabaseHandling {
+   protected Connection con;
+    
+    
+    abstract boolean conection(String filename,String user, String password)throws SQLException, ClassNotFoundException;
+    abstract void disconect();
+    
+        
     
 }
+
