d0a7c3fb322274e2e2a5f8760c59927bc6d73905
karlinsv <karlinsv@PCR04-010.spe-mch.de>
2013-02-07 13:32:10 +0100

    PTPServer initialized

--- JP2PFS/src/jp2pfs/server/PTPServer.java
+++ JP2PFS/src/jp2pfs/server/PTPServer.java
@@ -4,10 +4,42 @@
  */
 package jp2pfs.server;
 
+import java.net.SocketAddress;
+import java.util.List;
+import jp2pfs.client.PTPClient;
+
 /**
  *
  * @author karlinsv
  */
-public class PTPServer {
+public class PTPServer extends Thread {
+    
+    public PTPServer() {
+        
+    }
+    
+    public PTPClient addClient(String name, SocketAddress ip, int port) {
+        return null;
+    }
+    
+    public void updateClient(PTPClient client) {
+        
+    }
+    
+    public boolean removeClient(PTPClient client) {
+        return false;
+    }
+    
+    public PTPClient getClient(int id) {
+        return null;
+    }
+    
+    public List<PTPClient> getClients() {
+        return null;
+    }
+    
+    public List<PTPClient> getClientsOnline() {
+        return null;
+    }
     
 }
