474b5c1ba1b491214d362a8930c7e460e2320024
karlinsv <karlinsv@PCR04-010.spe-mch.de>
2013-02-07 10:31:20 +0100

    PTPServer added

---
+++ JP2PFS/src/jp2pfs/server/PTPServer.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp2pfs.server;
+
+/**
+ *
+ * @author karlinsv
+ */
+public class PTPServer {
+    
+}
