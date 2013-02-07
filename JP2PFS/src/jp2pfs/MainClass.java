ec24e85381cb2e52d72ee81d7f4aca011024af56
karlinsv <karlinsv@PCR04-010.spe-mch.de>
2013-02-07 10:01:42 +0100

    Main class added

--- JP2PFS/src/jp2pfs/MainClass.java
+++ JP2PFS/src/jp2pfs/MainClass.java
@@ -8,11 +8,10 @@ package jp2pfs;
  *
  * @author karlinsv
  */
-public class Main {
-
+public class MainClass {
     /**
-     * @param args the command line arguments
-     */
+    * @param args the command line arguments
+    */
     public static void main(String[] args) {
         // TODO code application logic here
     }
