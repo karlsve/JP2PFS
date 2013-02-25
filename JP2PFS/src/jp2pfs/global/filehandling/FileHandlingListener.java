1975cc96cbffe2fb720ab60aa7c09ecfc6ffcc89
karlinsv <karlinsv@PCR04-011.spe-mch.de>
2013-02-25 14:25:52 +0100

    - FileHandling changed

---
+++ JP2PFS/src/jp2pfs/global/filehandling/FileHandlingListener.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp2pfs.global.filehandling;
+
+/**
+ *
+ * @author karlinsv
+ */
+public interface FileHandlingListener {
+    public void onMessage(FileHandlingMessage message);
+}
